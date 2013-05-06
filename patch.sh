f63f429c8846f44f9048b38d3c9a0e8c91be13ad
Denis Fateyev <denis@fateyev.com>
2013-05-07 02:09:10 +0600

    seabios-1.7.2 compatibility and cleanup

--- patch.sh
+++ patch.sh
@@ -2,26 +2,11 @@
 #set -xv
 export HEADER="==========================================================================="
 
-
-echo
-echo $HEADER
-echo retrieve the latest seabios from upstream git repo.
-echo $HEADER
-echo
-
-git submodule init
-git submodule sync
-git submodule update
-
-cd seabios.submodule && git reset --hard && git clean -f; cd ..
-
-
 echo
 echo $HEADER
 echo "dump the slic from motherboard (root password required)"
 echo $HEADER
 echo
-
 
 # dump the slic table from the computer (note: requires root)
 sudo xxd -i /sys/firmware/acpi/tables/SLIC | grep -v len | sed 's/unsigned char.*/static char SLIC[] = {/' > seabios.submodule/src/acpi-slic.hex
@@ -51,4 +36,3 @@ echo
 
 cd ..
 ls seabios.submodule/out/bios.bin
-
